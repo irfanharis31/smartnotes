e52fe7f19b0cac4156a8beb1328b7b8d00bd6a1d
SHAIK FAKRUDDIN <shaikfakruddin2145@gmail.com>
2024-08-22 09:31:58 +0530

    modified app.jsx

--- smartnotes-frontend/src/App.jsx
+++ smartnotes-frontend/src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="flex justify-center items-center h-screen bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-500">
-        Hello, Tailwind CSS!
+        Hello, welcome to react
       </h1>
     </div>
   );
